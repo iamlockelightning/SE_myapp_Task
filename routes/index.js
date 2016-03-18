ace02ec256f0bd2c76222ae79ff120ffe0443b08
s0108 <iamlockelightning@gmail.com>
2016-03-18 16:23:10 +0800

    Added project.

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+// var Weibo = require('nodeweibo');
+// var setting = require('public/jsons/setting.json');
+// // 首次调用接口前需初始化Weibo类，传入配置信息 (appKey, appSecret, redirect_url, etc. )
+// Weibo.init(setting);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
